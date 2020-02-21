46cd589ca66ca47dca487477a485394145431002
mikhail-ershov <35760176+mikhail-ershov@users.noreply.github.com>
2020-02-21 12:18:08 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "manager.h"
-// exceptions are needed
+
 int main(int argc, char* argv[]){
     if (argc < 4) {
         printf("Not enough arguments\n");
