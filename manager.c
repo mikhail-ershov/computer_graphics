47426b6f326f73bbd234cbae76df77706f7c64dd
mikhail-ershov <35760176+mikhail-ershov@users.noreply.github.com>
2020-02-21 12:17:37 +0300

    Add files via upload

---
+++ manager.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "ppm.h"
+#include "pgm.h"
+
+void manageFile(char* inputFileName, char* outputFileName, int action) {
+    FILE* fin = fopen(inputFileName, "rb");
+    if (fin == NULL) {
+        printf("File can't be opened\n");
+        return;
+    }
+    char header[3];
+    fread(header, 1, 2, fin);
+    header[2] = '\0';
+    fclose(fin);
+    if (strcmp(header, "P5") == 0) {
+        PGM* pgm = readPGM(inputFileName);
+        switch(action){
+            case 0: inversePGM(pgm); writePGM(outputFileName, pgm);  break;
+            case 1: reflectPGM_H(pgm); writePGM(outputFileName, pgm); break;
+            case 2: reflectPGM_V(pgm); writePGM(outputFileName, pgm); break;
+            case 3: rotatePGM_R(pgm); writePGM(outputFileName, pgm); break;
+            case 4: rotatePGM_L(pgm); writePGM(outputFileName, pgm); break;
+            default: printf("Incorrect action\n"); break;
+        }
+        free(pgm);
+        return;
+    }
+    if (strcmp(header, "P6") == 0) {
+        PPM* ppm = readPPM(inputFileName);
+        switch(action) {
+            case 0: inversePPM(ppm); writePPM(outputFileName, ppm); break;
+            case 1: reflectPPM_H(ppm); writePPM(outputFileName, ppm); break;
+            case 2: reflectPPM_V(ppm); writePPM(outputFileName, ppm); break;
+            case 3: rotatePPM_R(ppm); writePPM(outputFileName, ppm); break;
+            case 4: rotatePPM_L(ppm); writePPM(outputFileName, ppm); break;
+            default: printf("Incorrect action\n"); break;
+        }
+        if (ppm != NULL) {
+            free(ppm->data);
+            free(ppm);
+        }
+        return;
+    }
+    printf("File is not a pbm file\n");
+}
---
+++ manager.h
@@ -0,0 +1,6 @@
+#ifndef MANAGER_H
+#define MANAGER_H
+
+void manageFile(char* inputFileName, char* outputFileName, int action);
+
+#endif
